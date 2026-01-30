d789692c84779800ca73d7547e573967171a8208
rsagunn <reilans737@deltalearns.ca>
2026-01-30 09:44:52 -0800

    Ability to enter scores and finish with 'done'
    Added score var for the scores that my user enters. Added a loop so if the user has an error in later code it will come back to step 1.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # ask user for list of scores
+scores = []  # list to hold scores
 
+# ask list of scores
+while True:  # loop
+    input_scores = input("Enter your score (or type 'done' to finish): ")  # get scores
 
+    # check if user done
+    if input_scores.lower() == "done":
+        break  # exit the loop
 
 # convert scores into grades
 
